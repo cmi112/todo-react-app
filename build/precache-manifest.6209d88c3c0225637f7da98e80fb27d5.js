dd08d6f441d64d0b4629b3a599c6db11a7dcda37
A.E <a.e@AEs-MacBook-Air.local>
2021-05-06 10:13:29 +0200

    version 1

---
+++ build/precache-manifest.6209d88c3c0225637f7da98e80fb27d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc4d10521014aafa5494a79f0761736",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37ac7228405ba96c7ea0",
+    "url": "./static/css/main.eb490982.chunk.css"
+  },
+  {
+    "revision": "21980e29b1a6ab55057a",
+    "url": "./static/js/2.f7759ccf.chunk.js"
+  },
+  {
+    "revision": "37ac7228405ba96c7ea0",
+    "url": "./static/js/main.f8fa9d33.chunk.js"
+  },
+  {
+    "revision": "baab4f6995ba25947e6f",
+    "url": "./static/js/runtime-main.f5f70450.js"
+  }
+]);
